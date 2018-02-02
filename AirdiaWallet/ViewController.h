a86e1e4beccceeefa27dd472e9aed37adf9680b0
muma <zhengyuanqian@zhongan.io>
2018-02-02 17:04:13 +0800

    Initial Commit

---
+++ AirdiaWallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirdiaWallet
+//
+//  Created by muma on 2018/2/2.
+//  Copyright © 2018年 muma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
